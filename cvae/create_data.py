6ce95ec49025a44144cc8ed18a0aa927e7f2fe45
thecoldviews <sarthakahuja@outlook.com>
2019-11-16 22:37:22 -0500

    added conditioning variables

--- cvae/create_data.py
+++ cvae/create_data.py
@@ -2,11 +2,57 @@ import json
 import argparse
 import os
 import numpy as np
+import shutil
+import itertools
+
+def get_walls(complete_env_path):
+    with open(complete_env_path, "r") as e:
+        line = e.readline()
+        walls = []
+        count = 0
+        while line:
+            if "wall" in line:
+                wall = line.strip(" ").strip("\n").split(" ")[1:]
+                walls.extend(wall)
+                count = count + 1
+            
+            line = e.readline()
+        
+        # padding
+        while count < 20:
+            walls.extend(["0"] * 6)
+            count = count + 1
+        
+        
+    return walls
+
+def get_start_goal(complete_sample_path):
+    with open(complete_sample_path, "r") as f:
+        
+        line = f.readline()
+        line = f.readline()
+        
+        start_angles = line.strip("\n").strip(" ").split(" ")[:2]
+
+        for last in f:
+            pass
+        
+        goal_angles = last.strip("\n").strip(" ").split(" ")[:2]
+        
+    return start_angles, goal_angles
+                    
 
 def label_traj(complete_sample_path, complete_env_path, complete_directory_clean, data_path_arm, data_path_base):
     '''
         get arm and base labels from a single solution_path file
     '''
+    
+    # create conditioning variable
+    walls = get_walls(complete_env_path)
+    start, goal = get_start_goal(complete_sample_path)
+    
+    conditions = " ".join(start + goal + walls)
+    
     with open(data_path_base, "a") as b:
         with open(data_path_arm, "a") as a:
             with open(complete_sample_path, "r") as f:
@@ -18,8 +64,6 @@ def label_traj(complete_sample_path, complete_env_path, complete_directory_clean
                 line = f.readline()
                 
                 while line:
-                    # import pdb
-                    # pdb.set_trace()
                     
                     joint_angles = line.strip("\n").strip(" ").split(" ")
                     joint_angles = list(map((lambda x: float(x)),joint_angles))
@@ -38,14 +82,15 @@ def label_traj(complete_sample_path, complete_env_path, complete_directory_clean
                     arm_moved = np.any(np.abs(arm_joint_angles - prev_arm_joint_angles) > epsilon)
                     
                     if base_moved:
-                        b.write("{} {}\n".format(joint_angles[0], joint_angles[1]))
+                        b.write("{} {} {}\n".format(joint_angles[0], joint_angles[1], conditions))
                     
                     if arm_moved:
-                        a.write("{} {}\n".format(joint_angles[0], joint_angles[1]))
+                        a.write("{} {} {}\n".format(joint_angles[0], joint_angles[1], conditions))
                         
                     line = f.readline()
                     prev_base_joint_angles = base_joint_angles
                     prev_arm_joint_angles = arm_joint_angles
+                    
 
 def parse_arguments():
     '''
@@ -69,9 +114,10 @@ if __name__ == "__main__":
     else:
         directory = os.path.join(os.environ["PLANNING_PROJECT_DATA"], "train")
         directory_clean = os.path.join(os.environ["PLANNING_PROJECT_DATA"], "train_clean")
-            
+    
+    # remove current results
     if os.path.exists(directory_clean):
-        os.remove(directory_clean)
+        shutil.rmtree(directory_clean)
         
     os.mkdir(directory_clean)
     
